59c51c79b862605e03a96abae2ef6defb08419f8
Geovana Cardoso <geovanamscardoso183@gmail.com>
2025-09-02 10:28:12 -0300

    Corrigir server.js

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,21 @@ app.use(cors());
 
 const Port = 3001;
 
+const imagens = [
+  {url: '/images/1-Tesla-Model-32.jpg', legenda: 'Tesla Model 3 — Foto: divulgação/Tesla '},
+  {url: '/images/seres3.avif', legenda: 'Seres 3 — Foto: divulgação/Seres'},
+  {url: '/images/konaEv.avif', legenda: 'Hyundai Kona EV — Foto: divulgação/Hyundai'},
+  {url: '/images/dolphin.jpg', legenda: 'BYD Dolphin Plus — Foto: divulgação/BYD '},
+  {url: '/images/jaceJS1.avif', legenda: 'JAC e-JS1 — Foto: divulgação/JAC'}];
+
+let imagemAtual = 0;
+    
 app.get("/carros", (req, res) => {
   res.json({
-    //Declarando o array de imagens
-    imagens: [
-        '/images/1-Tesla-Model-32.jpg',
-        '/images/seres3.avif',
-        '/images/konaEv.avif',
-        '/images/dolphin.jpg',
-        '/images/jaceJS1.avif'
-    ],
-
-    //Declarando array de legendas de cada imagem
-    legendas: ['Tesla Model 3 — Foto: divulgação/Tesla ',
-        'Seres 3 — Foto: divulgação/Seres',
-        'Hyundai Kona EV — Foto: divulgação/Hyundai',
-        'BYD Dolphin Plus — Foto: divulgação/BYD ',
-        'JAC e-JS1 — Foto: divulgação/JAC'
-    ]});
+    url: imagens[imagemAtual].url, legenda:imagens[imagemAtual].legenda
 });
+
+imagemAtual = (imagemAtual + 1) % imagens.length;});
 
 app.listen(Port, ()=>{
     console.log(`Servidor Rodando http://localhost:${Port}`)
